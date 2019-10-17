0a48b0ad8263b9e377cf4758179e75bbbbf94ce0
y-miyazaki <ymiyazakixyz@gmail.com>
2019-10-17 17:00:23 +0900

    fixed .gitignore. fixed devcontainer.json.

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+    "rsc.io/quote"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World")
+	fmt.Println(quote.Opt())
 }
 
 func main() {
